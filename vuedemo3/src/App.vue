e299799c686145794af6d2ffa91ead6464104ba3
min <katrina77@126.com>
2019-09-03 08:53:39 +0800

    add

--- vuedemo3/src/App.vue
+++ vuedemo3/src/App.vue
@@ -2,12 +2,27 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/user">user</router-link> -->
+      <button @click="gotoUser">user</button>
+      
+
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  data(){
 
+  },
+  methods:{
+    gotoUser(){
+      this.$router.push('/user')
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
