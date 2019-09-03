810d47e6ae77f3eeb46b8d6cd30695948439648d
min <katrina77@126.com>
2019-09-03 18:08:04 +0800

    add

--- vuedemo3/src/main.js
+++ vuedemo3/src/main.js
@@ -2,9 +2,46 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Cookies from 'js-cookie'
 import './registerServiceWorker'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+// Create a cookie, valid across the entire site:
+Cookies.set('name', 'value');
+
+// Create a cookie that expires 7 days from now, valid across the entire site:
+Cookies.set('name', 'value', { expires: 7 });
+
+// Create an expiring cookie, valid to the path of the current page:
+Cookies.set('name', 'value', { expires: 7, path: '' });
+
+
+// Read cookie:
+Cookies.get('name'); // => 'value'
+Cookies.get('nothing'); // => undefined
+
+// Read all visible cookies:
+Cookies.get(); // => { name: 'value' }
+
+// Delete cookie:
+Cookies.remove('name');
+
+// Delete a cookie valid to the path of the current page:
+Cookies.set('name', 'value', { path: '' });
+Cookies.remove('name'); // fail!
+Cookies.remove('name', { path: '' }); // removed!
+
+
+// 取cookie时需要转成对象
+const user = {
+  name: 'lia',
+  age: 18
+}
+Cookies.set('user', user)
+const liaUser = Cookies.getJSON('user')
 
 new Vue({
   router,
